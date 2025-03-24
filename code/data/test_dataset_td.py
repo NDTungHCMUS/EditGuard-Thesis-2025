fc86a9f835bcdac673f645f46631af388e95e981
NDTung_HCMUS <94626904+NDTungHCMUS@users.noreply.github.com>
2025-03-25 00:26:22 +0700

    Fix configuration

--- code/data/test_dataset_td.py
+++ code/data/test_dataset_td.py
@@ -36,7 +36,7 @@ class imageTestDataset(data.Dataset):
         img_GT = util.read_img(None, osp.join(parent_path_GT, f"{index}.png"))
         img_GT = img_GT[:, :, [2, 1, 0]]
         img_GT = torch.from_numpy(np.ascontiguousarray(np.transpose(img_GT, (2, 0, 1)))).float().unsqueeze(0)
-        img_GT = torch.nn.functional.interpolate(img_GT, size=(512, 512), mode='nearest', align_corners=None)
+        # img_GT = torch.nn.functional.interpolate(img_GT, size=(512, 512), mode='nearest', align_corners=None)
 
         T, C, W, H = img_GT.shape
         list_h = []
@@ -63,4 +63,4 @@ class imageTestDataset(data.Dataset):
             }
     
     def __len__(self):
-        return len(self.image_list_gt * 36)  
+        return len(self.image_list_gt * 4)  
