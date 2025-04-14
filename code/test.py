00f7dd8a64f9642f5761c8b20dd5bd1541be0131
NDTung_HCMUS <94626904+NDTungHCMUS@users.noreply.github.com>
2025-04-14 21:13:33 +0700

    Fix test file

--- code/test.py
+++ code/test.py
@@ -97,6 +97,7 @@ def main():
     ## Explaination: Split images into n^2 sub-images (comment if already done)
     split_all_images(input_folder = opt['datasets']['TD']['data_path'],
                      output_folder = opt['datasets']['TD']['split_path_ori'],
+                     num_images = opt['datasets']['TD']['num_images'],
                      num_child_on_width_size = opt['datasets']['TD']['num_child_on_width_size'],
                      num_child_on_height_size = opt['datasets']['TD']['num_child_on_height_size'])
     # ----- VN End -----
