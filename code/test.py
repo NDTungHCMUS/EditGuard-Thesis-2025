c171bdef89f772464c4f29038301e8afe066167f
NDTung_HCMUS <94626904+NDTungHCMUS@users.noreply.github.com>
2025-04-25 20:44:37 +0700

    Fix configuration

--- code/test.py
+++ code/test.py
@@ -96,11 +96,12 @@ def main():
 
     # ----- VN Start -----
     ## Explaination: Split images into n^2 sub-images (comment if already done)
-    split_all_images(input_folder = opt['datasets']['TD']['data_path'],
-                     output_folder = opt['datasets']['TD']['split_path_ori'],
-                     num_images = opt['datasets']['TD']['num_images'],
-                     num_child_on_width_size = opt['datasets']['TD']['num_child_on_width_size'],
-                     num_child_on_height_size = opt['datasets']['TD']['num_child_on_height_size'])
+    if (opt['datasets']['TD']['need_to_split']):
+        split_all_images(input_folder = opt['datasets']['TD']['data_path'],
+                        output_folder = opt['datasets']['TD']['split_path_ori'],
+                        num_images = opt['datasets']['TD']['num_images'],
+                        num_child_on_width_size = opt['datasets']['TD']['num_child_on_width_size'],
+                        num_child_on_height_size = opt['datasets']['TD']['num_child_on_height_size'])
     # ----- VN End -----
 
     # Create train and val dataloader
